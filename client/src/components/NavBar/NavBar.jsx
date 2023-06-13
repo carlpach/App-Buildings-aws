675e5cf7ad238d02e881014fec3ceac93575ffa3
carlos <carlos.pacheco@bootcamp-upgrade.com>
2023-06-13 08:13:19 +0200

    navbar to sidebar and icons

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,20 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom"
+import MaterialIcon, {colorPalette} from 'material-icons-react';
 
 const NavBar = () => {
   return (
     <div className="b-navbar">
-      <Link to="/">HOME</Link>
-      <Link to="/">LOGOUT</Link>
+      <Link to="/">
+        <span class="material-icons">home</span>
+      </Link>
+      <Link to="/">
+        <span class="material-icons">person</span>
+      </Link>
+      <Link to="/">
+        <span class="material-icons">logout</span>
+      </Link>
+
     </div>
   )
 }
