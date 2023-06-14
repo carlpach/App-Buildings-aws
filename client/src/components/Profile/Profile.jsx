201861c443f498d62eba8eabdb13d7b5a77f2a92
carlos <carlos.pacheco@bootcamp-upgrade.com>
2023-06-14 11:21:38 +0200

    auth front

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -2,19 +2,24 @@ import Gallery from "../Gallery/Gallery";
 import "./Profile.css"
 
 const Profile = ( { buildings, user }) => {
+  console.log("Profile running ------->");
+  console.log(user);
     
   const buildingsUser = buildings.filter((buildg) => (
-    user.properties.includes(buildg._id) 
+    user.user.properties.includes(buildg._id) 
   ))
   
   console.log("buildingsUser------", buildingsUser);
-  
+
+  const placeholderImg = "https://www.mountsinai.on.ca/wellbeing/our-team/team-images/person-placeholder/image";
+
    return (
     <div className="b-profile">
-      <img src={user.image} alt="person"/>
-      <h2>{user.name} {user.surname}</h2>
+      <img src={user.user.image ? user.user.image : placeholderImg} alt="person"/>
+      <h2>{user.user.userName}</h2>
+      <h2>{user.user.email}</h2>
       <h3>My buildings</h3>
-      <Gallery buildings = {buildingsUser} />
+      <Gallery buildings = {buildingsUser} flagAdd = {false} />
     </div>
   )
 }
